669e2390320edae29175a57030b6884f3903d707
Brent Ahrens <cidida@gmail.com>
2022-05-25 15:26:19 -0700

    remove need for Image tag

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         <h1>physician-centric</h1>
         <h1>partnership</h1>
         <p>We <span>partner</span> with independent cardiology groups <span>to invest</span> in their long-term success with capital, staff, technology, expertise, and best practices <span>to excel</span> in a rapidly changing environment and deliver superior clinical outcomes.</p>
-        <button><a href="mailto:info@cardioone.com" target="_blank">Contact Us</a></button>
+        <button><a href="mailto:info@cardioone.com" target="_blank" rel="noreferrer">Contact Us</a></button>
       </div>
     </div>
   )
