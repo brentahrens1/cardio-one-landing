9ea1ae14c93c0510756e57f09ea21be0798a1323
Brent Ahrens <cidida@gmail.com>
2022-06-03 12:27:19 -0700

    updated form

--- components/Form.js
+++ components/Form.js
@@ -1,60 +1,71 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 
 const ContactUs = () => {
   const form = useRef();
+  const [ didSend, setDidSend ] = useState(false)
 
-//   const sendEmail = (e) => {
-//     e.preventDefault();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-//     emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'YOUR_PUBLIC_KEY')
-//       .then((result) => {
-//           console.log(result.text);
-//       }, (error) => {
-//           console.log(error.text);
-//       });
-//   };
+    emailjs.sendForm('service_gdpxq75', 'template_eselfa8', form.current, 'oGkSHj6RyQNqR-FTT')
+      .then((result) => {
+          console.log(result.text);
+          setDidSend(true)
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
 
   return (
-    <form ref={form}>
-      <div className='names'>
+    <>
+    {
+        didSend ? 
         <div>
-            <label>First Name</label>
-            <input type="text" name="first_name" required />
+            <h2>Thank you, we received your request.</h2>
         </div>
-        <div>
-            <label>Last Name</label>
-            <input type="text" name="last_name" required />
-        </div>
-      </div>
-      <div className='names'>
-        <div>
-            <label>Email</label>
-            <input type="text" name="last_name" required />
-        </div>
-        <div>
-            <label>Phone Number</label>
-            <input type="text" name="last_name" required />
-        </div>
-      </div>
-      <label>Preferred method of contact</label>
-      <div className='checkboxes'>
-        <label className="container">Email
-            <input type="radio" name="radio" />
-            <span className="checkmark"></span>
-        </label>
-        <label className="container">Phone
-            <input type="radio" name="radio" />
-            <span className="checkmark"></span>
-        </label>
-      </div>
-      {/* <input type="text" name="user_email" /> */}
-      <label>Clinic Name</label>
-      <input type="text" name="user_email" required />
-      <label>Interest in speaking with CardioOne</label>
-      <textarea name="message" required />
-      <input type="submit" value="Send" />
-    </form>
+        :
+        <form ref={form} onSubmit={sendEmail}>
+            <div className='names'>
+                <div>
+                    <label>First Name</label>
+                    <input type="text" name="first_name" required />
+                </div>
+                <div>
+                    <label>Last Name</label>
+                    <input type="text" name="last_name" required />
+                </div>
+            </div>
+            <div className='names'>
+                <div>
+                    <label>Email</label>
+                    <input type="email" name="email" required />
+                </div>
+                <div>
+                    <label>Phone Number</label>
+                    <input type="text" name="phone" required />
+                </div>
+            </div>
+            <label>Preferred method of contact</label>
+            <div className='checkboxes'>
+                <label className="container">Email
+                    <input type="radio" name="radio-1" />
+                    <span className="checkmark"></span>
+                </label>
+                <label className="container">Phone
+                    <input type="radio" name="radio-2" />
+                    <span className="checkmark"></span>
+                </label>
+            </div>
+            {/* <input type="text" name="user_email" /> */}
+            <label>Clinic Name</label>
+            <input type="text" name="clinic" required />
+            <label>Interest in speaking with CardioOne</label>
+            <textarea name="message" required />
+            <input type="submit" value="Send" />
+        </form>
+    }
+    </>
   );
 };
 
