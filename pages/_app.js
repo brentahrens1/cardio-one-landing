0fa969ac6ab3a43f856685f940cd270c63703d8a
Brent Ahrens <cidida@gmail.com>
2022-07-14 15:38:36 -0700

    add google tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
 import '../styles/index.scss'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        id="google-tag"
+        strategy="lazyOnload"
+      >
+        {
+          `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-T82592N');`
+        }
+      </Script>
+      <Component {...pageProps} />
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T82592N"
+      height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
+    </>
+  )
 }
 
 export default MyApp
